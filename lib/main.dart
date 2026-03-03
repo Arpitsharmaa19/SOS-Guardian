167cd9b334a81af8bc160c33c3daf19b7de44266
arpit sharma <SS@MacBook-Air.local>
2026-03-03 21:58:45 +0530

    Fix splash screen import by using relative path

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:VoiceAid/screens/splash_screen.dart';
+import 'screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
