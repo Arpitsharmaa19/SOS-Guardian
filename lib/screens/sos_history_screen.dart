ac1bd84f5a6ca43156ceefa8a219b268fd8b63aa
arpit sharma <SS@MacBook-Air.local>
2026-03-24 14:55:50 +0530

    🔧 Fix: Resolved import issues in SOS History Cloud sync

--- lib/screens/sos_history_screen.dart
+++ lib/screens/sos_history_screen.dart
@@ -1,3 +1,6 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:animate_do/animate_do.dart';
